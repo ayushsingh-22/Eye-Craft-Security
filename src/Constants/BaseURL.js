864d7e676afd266b74f2a0108aa261b0e52c9929
ayushsingh-22 <ayushkumar2205@gmail.comm>
2025-06-24 16:39:44 +0530

    Update baseURL to use production server URL

--- src/Constants/BaseURL.js
+++ src/Constants/BaseURL.js
@@ -1,3 +1,3 @@
-const baseURL = "http://localhost:8080"; // No leading space, direct assignment
+const baseURL = "https://server-saby.onrender.com"; // No leading space, direct assignment
 
 export default baseURL;
